973f3a10d97baddbda449c53e771cff8daa4cc12
Glitch (alix) <none>
2020-09-01 09:49:11 +0000

    🐂🐽 Checkpoint ./views/form.php:15319430/404

--- views/form.php
+++ views/form.php
@@ -3,5 +3,15 @@
   $visitor_email = $_POST["email"];
   $message = $_POST["message"]
     
-  $email_from = "A"
+  $email_from = "support@alix.glitch.me";
+  $email_subject = "New Form Contact Us";
+  $email_body = "User Name: $name.\n"
+                "User Email: $visitor_email. \n"
+                "User Message: $message. \n";
+
+  $to = "ayaanatri@gmail.com";
+  $headers = "Form $email_form \r\n";
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  
 ?>
